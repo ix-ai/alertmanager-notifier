87f3867247867ba8ebd0c3b03affb81677f48c28
Alex Thomae <alex@thom.ae>
2020-02-09 08:45:58 +0100

    Adds support for gelf logging

--- alertmanager-notifier/__init__.py
+++ alertmanager-notifier/__init__.py
@@ -8,4 +8,7 @@ from .lib import log as logging
 log = logging.setup_logger(
     name=__package__,
     level=os.environ.get('LOGLEVEL', 'INFO'),
+    gelf_host=os.environ.get('GELF_HOST'),
+    gelf_port=int(os.environ.get('GELF_PORT', 12201)),
+    _ix_id=os.environ.get(__package__),
 )
